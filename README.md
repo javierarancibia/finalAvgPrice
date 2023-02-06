3cf70d8bbe2050395dbab6617c80ab2ff9a71257
javierarancibia <69688629+javierarancibia@users.noreply.github.com>
2023-02-06 16:48:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# finalAvgPrice
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-th8zfd)
---
+++ index.js
@@ -0,0 +1,12 @@
+const finalAvgPrice = (array) => {
+  const filteredArray = array.filter((x) => x !== 0);
+  console.log(filteredArray)
+  const sum = filteredArray.reduce((prev, acc) => prev + acc, 0);
+  const average = sum / filteredArray.length;
+  if (isNaN(average)) {
+    return 0
+  } 
+  return average
+};
+
+console.log(finalAvgPrice([0, 2, 4, 0, 0, 8]));
